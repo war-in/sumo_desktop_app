091c8e7a51b8cd3cf913a217f4ae5d505657f1fc
Barbara Kulawska <kulawskab@student.agh.edu.pl>
2022-08-22 22:08:41 +0200

    Fixed bug with get and post requests

--- src/components/Weighting/Weighting.tsx
+++ src/components/Weighting/Weighting.tsx
@@ -12,8 +12,6 @@ const competitionId = 1;
 const desktopServerUrl = "http://localhost:8080/";
 
 //TODO: the 'club' field is missing in data received from the endpoint - add the field or remove club info from front
-//TODO: fix: GET for competitor details takes two clicks on row instead of one
-//TODO: fix: POST for weighting details takes two button clicks instead of one
 
 function Weighting() {
     const [personalDetails, setPersonalDetails] = useState(() => new PersonalDetails(
@@ -54,29 +52,61 @@ function Weighting() {
     ]);
     const [weight, setWeight] = useState(null);
     const [weightingDetails, setWeightingDetails] = useState([]);
+    const [competitorsData, setCompetitorsData] = useState([{
+        "id": null,
+        "personalDetails": {
+            "id": null,
+            "name": null,
+            "surname": null,
+            "phoneNumber": null,
+            "linkToProfilePicture": undefined,
+            "birthDate": [
+                null,
+                null,
+                null
+            ],
+            "sex": {
+                "sex": null
+            }
+        },
+        "status": null,
+        "country": null
+    }]);
+
+    let dict = {};
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await axios.get(desktopServerUrl + `weighting/competitors?competitionId=` + competitionId)
+                .then(response => {
+                    setCompetitorsData(response.data);
+                })
+        }
+        fetchData()
+    }, []);
 
     function Competitors() {
 
-        const [competitorsData, setCompetitorsData] = useState([{
-            "id": null,
-            "personalDetails": {
-                "id": null,
-                "name": null,
-                "surname": null,
-                "phoneNumber": null,
-                "linkToProfilePicture": undefined,
-                "birthDate": [
-                    null,
-                    null,
-                    null
-                ],
-                "sex": {
-                    "sex": null
+        useEffect(() => {
+            const fetchData = async () => {
+                for (const comp of competitorsData) {
+                    let cat = null;
+                    await axios.get(desktopServerUrl + `weighting/categories?competitionId=` + competitionId + `&competitorId=` + comp.personalDetails.id)
+                        .then(response => {
+                            cat = response.data;
+                        })
+                    let wd = [];
+                    for (const category of cat) {
+                        await axios.get(desktopServerUrl + `weighting/weighting-details?categoryAtCompetitionId=` + category.id + `&competitorId=` + comp.personalDetails.id)
+                            .then(response => {
+                                wd.push(response.data)
+                            })
+                    }
+                    dict[comp.personalDetails.id] = { categories: cat, weightDetails: wd }
                 }
-            },
-            "status": null,
-            "country": null
-        }]);
+            }
+            fetchData()
+        }, []);
 
         const Columns = [
             {"title": "Name", "field": "personalDetails.name"},
@@ -85,13 +115,6 @@ function Weighting() {
             {"title": "Country", "field": "country"},
             {"title": "Sex", "field": "personalDetails.sex.sex"}
         ]
-
-        useEffect(() => {
-            axios.get(desktopServerUrl + `weighting/competitors?competitionId=` + competitionId)
-                .then(response => {
-                    setCompetitorsData(response.data);
-                })
-        }, []);
 
         return (
             <MaterialTable
@@ -102,21 +125,11 @@ function Weighting() {
                     doubleHorizontalScroll: true,
                     maxBodyHeight: 250
                 }}
-                onRowClick={async (_event, rowData) => {
+                onRowClick={(_event, rowData) => {
                     if (rowData == null) return
-                    weightingDetails.length = 0
-                    await axios.get(desktopServerUrl + `weighting/categories?competitionId=` + competitionId + `&competitorId=` + rowData.personalDetails.id)
-                        .then(response => {
-                            setCategories(response.data)
-                        })
-                    for (const category of categories) {
-                        await axios.get(desktopServerUrl + `weighting/weighting-details?categoryAtCompetitionId=` + category.id + `&competitorId=` + rowData.personalDetails.id)
-                            .then(response => {
-                                weightingDetails.push(response.data)
-                                console.log(weightingDetails)
-                            })
-                    }
-                    setWeight(weightingDetails[0].weight)
+                    setCategories(dict[rowData.personalDetails.id].categories);
+                    setWeightingDetails(dict[rowData.personalDetails.id].weightDetails);
+                    setWeight(dict[rowData.personalDetails.id].weightDetails[0].weight)
                     setPersonalDetails(new PersonalDetails(rowData.personalDetails.id,
                         rowData.personalDetails.name, rowData.personalDetails.surname,
                         rowData.personalDetails.linkToProfilePicture, rowData.personalDetails.birthDate[0],
@@ -168,16 +181,12 @@ function Weighting() {
                     <Row><Input id="weightInput" className="detail" defaultValue={weight}></Input></Row>
                     <Row className="button">
                         <Button onClick={async () => {
-                            setWeight(await document.getElementById("weightInput").value)
-                            setCompetitor(new Competitor(competitor.id, personalDetails,
-                                competitor.country, competitor.club, weight))
+                            dict[personalDetails.id].weightDetails.weight = Number(await document.getElementById("weightInput").value);
                             for (const wd of weightingDetails) {
-                                console.log(weightingDetails)
                                 let body = wd
-                                body.weight = Number(weight)
+                                body.weight = dict[personalDetails.id].weightDetails.weight
                                 console.log(body)
                                 await axios.post(desktopServerUrl + `weighting/update-weighing-details`, body)
-                                    .then(response => console.log(response.data))
                             }
                         }}>OK</Button>
                     </Row>
